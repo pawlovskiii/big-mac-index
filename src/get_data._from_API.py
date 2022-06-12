5e613eced422be000facbf9420eb600e48ef542b
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-06-13 00:04:29 +0200

    Put code extracting data from api to the function

--- src/get_data._from_API.py
+++ src/get_data._from_API.py
@@ -12,8 +12,11 @@ def json_to_csv(data, countryCode):
     read_data.to_csv(rf"bigmac_index/{countryCode}.csv", index=False)
 
 
-for countryCode in countryCodeList: 
-    response = requests.get(f"https://data.nasdaq.com/api/v3/datasets/ECONOMIST/{countryCode}?api_key=Hy29GkkAwX2gVmj1wpLh")
-    data = json.dumps(response.json())
-    json_to_csv(data, countryCode)
-    print(f"{countryCode} saved successfully!")
+def get_data():
+    for countryCode in countryCodeList:
+        response = requests.get(
+            f"https://data.nasdaq.com/api/v3/datasets/ECONOMIST/{countryCode}?api_key=Hy29GkkAwX2gVmj1wpLh"
+        )
+        data = json.dumps(response.json())
+        json_to_csv(data, countryCode)
+        print(f"{countryCode} saved successfully!")
