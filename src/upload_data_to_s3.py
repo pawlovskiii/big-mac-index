093ccd293f31c313a79c28196de78111adcdd8ce
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-06-13 12:30:36 +0200

    Move functions to main file to enable module separation in the following steps

--- src/upload_data_to_s3.py
+++ src/upload_data_to_s3.py
@@ -1,21 +1,21 @@
-import pandas as pd
-import requests
-import json
-from src.extract_metadata import countryCodeList
-from src.upload_data import upload_data
+# import pandas as pd
+# import requests
+# import json
+# from src.extract_metadata import countryCodeList
+# from src.upload_data import upload_data
 
 
-def json_to_csv(response, countryCode):
-    data = json.dumps(response.json())
-    read_data = pd.read_json(data)
-    read_data.to_csv(rf"bigmac_index/{countryCode}.csv", index=False)
-    print(f"{countryCode} saved successfully!")
-    upload_data(countryCode)
+# def json_to_csv(response, countryCode):
+#     data = json.dumps(response.json())
+#     read_data = pd.read_json(data)
+#     read_data.to_csv(rf"bigmac_index/{countryCode}.csv", index=False)
+#     print(f"{countryCode} saved successfully!")
+#     upload_data(countryCode)
 
 
-def extract_data_from_api():
-    for countryCode in countryCodeList[60:]:
-        response = requests.get(
-            f"https://data.nasdaq.com/api/v3/datasets/ECONOMIST/{countryCode}?api_key=Hy29GkkAwX2gVmj1wpLh"
-        )
-        json_to_csv(response, countryCode)
+# def extract_data_from_api():
+#     for countryCode in countryCodeList[60:]:
+#         response = requests.get(
+#             f"https://data.nasdaq.com/api/v3/datasets/ECONOMIST/{countryCode}?api_key=Hy29GkkAwX2gVmj1wpLh"
+#         )
+#         json_to_csv(response, countryCode)
