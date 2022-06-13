5757c515b228c09699b21d45e8752c5755273c67
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-06-13 16:28:22 +0200

    Extract the path to the config file

--- src/local_storage.py
+++ src/local_storage.py
@@ -1,10 +1,15 @@
 import json
 import pandas as pd
 from src.upload_data_to_s3 import upload_data
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read("config.ini")
+
 
 def save_data_local(response, countryCode):
     data = json.dumps(response.json())
     read_data = pd.read_json(data)
-    read_data.to_csv(rf"bigmac_index/{countryCode}.csv", index=False)
+    read_data.to_csv(rf"{config['DEFAULT']['path']}{countryCode}.csv", index=False)
     print(f"{countryCode} saved successfully!")
     upload_data(countryCode)
--- src/upload_data_to_s3.py
+++ src/upload_data_to_s3.py
@@ -1,4 +1,8 @@
 import boto3
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read("config.ini")
 
 # Create an S3 access object
 s3 = boto3.client("s3")
@@ -6,8 +10,8 @@ s3 = boto3.client("s3")
 
 def upload_data(countryCode):
     s3.upload_file(
-        Filename=f"bigmac_index/{countryCode}.csv",
+        Filename=f"{config['DEFAULT']['path']}{countryCode}.csv",
         Bucket="nasdaq-data-bucket",
-        Key=f"bigmac_index/{countryCode}.csv",
+        Key=f"{config['DEFAULT']['path']}{countryCode}.csv",
     )
     print(f"{countryCode}.csv uploaded successfully!")
