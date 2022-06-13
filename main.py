50ef526759079771d9e899ed44833c6d09b1b415
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-06-13 12:43:39 +0200

    Create a separate module for extracting data from URL API

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import pandas as pd
-import requests
-from src.extract_metadata import countryCodeList
-from src.local_storage import save_data_local
-
-
-def extract_data_from_api():
-    for countryCode in countryCodeList[60:]:
-        response = requests.get(
-            f"https://data.nasdaq.com/api/v3/datasets/ECONOMIST/{countryCode}?api_key=Hy29GkkAwX2gVmj1wpLh"
-        )
-        save_data_local(response, countryCode)
+from src.extract_data_from_api import extract_data_from_api
 
 
 def main():
