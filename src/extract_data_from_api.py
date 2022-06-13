38ed61f4106a24ae0081fb1dded29e1cf835f13b
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-06-13 16:14:04 +0200

    Create a separate file for data to increase the navigation through the project

--- src/extract_data_from_api.py
+++ src/extract_data_from_api.py
@@ -1,10 +1,16 @@
 import requests
+from configparser import ConfigParser
 from src.extract_metadata import countryCodeList
 from src.local_storage import save_data_local
+
+
+config = ConfigParser()
+config.read("config.ini")
+
 
 def extract_data_from_api():
     for countryCode in countryCodeList[60:]:
         response = requests.get(
-            f"https://data.nasdaq.com/api/v3/datasets/ECONOMIST/{countryCode}?api_key=Hy29GkkAwX2gVmj1wpLh"
+            f"https://data.nasdaq.com/api/v3/datasets/ECONOMIST/{countryCode}?api_key={config['DEFAULT']['api_key']}"
         )
         save_data_local(response, countryCode)
