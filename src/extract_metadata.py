48e78c3a75f91e7e8ba4c5a6355e2488f7032877
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-06-21 15:00:42 +0200

    Create a separate folder for metadata

--- src/extract_metadata.py
+++ src/extract_metadata.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 
 def get_country_code() -> List[str]:
-    metadata = pd.read_csv(r"ECONOMIST_metadata.csv")
+    metadata = pd.read_csv(r"./metadata/ECONOMIST_metadata.csv")
     countryCodeList = metadata["code"].tolist()
     return countryCodeList
